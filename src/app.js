14ad94727bae7d14322fb9f2c668924793f447d5
smoore2022 <rui22002@byui.edu>
2024-04-08 22:49:50 -0600

    api integration updated

--- src/app.js
+++ src/app.js
@@ -1,16 +1,17 @@
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+searchCity("Paris");
 
 function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
+  14;
   let cityElement = document.querySelector("#city");
 
   cityElement.innerHTML = response.data.city;
