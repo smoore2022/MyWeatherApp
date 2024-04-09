604e6fb770c05a73dc41d717374d32009d5c644e
smoore2022 <rui22002@byui.edu>
2024-04-09 00:02:51 -0600

    Icon updated

--- src/app.js
+++ src/app.js
@@ -18,12 +18,14 @@ function refreshWeather(response) {
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   timeElement.innerHTML = formatDate(date);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 
   temperatureElement.innerHTML = Math.round(temperature);
 }
